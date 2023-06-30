78535bb82c5a6948325e83c9b21c959430589ff4
Eldho N Joseph <eldhonj30@gmail.com>
2023-06-30 14:08:52 +0530

    DB changed to Atlas

--- config/connection.js
+++ config/connection.js
@@ -5,12 +5,14 @@ const state = {
 
 
 module.exports.connect = function(done){
-  const url = 'mongodb://127.0.0.1:27017/:27017';
+  // const url = 'mongodb://127.0.0.1:27017/:27017';
+    const url = 'mongodb+srv://eldhonj30:A1goritham@cluster0.v71lbwt.mongodb.net/'
+
   const dbname = 'shopping' ;
 
   MongoClient.connect(url, (err, data) => {
     if(err) return done(err);
-    state.db = data.db(dbname);
+    state.db = data.db(dbname); 
     done();
   })
 }
